ecae24b14c0af6646348529122295a19332a9759
Ofek Atar <ofekatar98@gmail.com>
2021-01-12 17:23:47 +0200

    Added kubernetes files.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,9 +2,13 @@ import express from 'express';
 import { json } from 'body-parser';
 
 const app = express();
-app.use(json);
+app.use(json());
 
 const port = 3000;
+
+app.get("/api/users/currentUser", (_, res) => {
+    res.send("Hello world.");
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`);
